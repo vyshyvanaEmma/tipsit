6c91f8e20fde5a6cf94dc05a30fb12e9db5d6aa8
lai2-24 <lai2-24@itismeucci.com>
2025-11-25 13:34:21 +0100

    http_req_res_manipulations: add 301 and 404 (correct)

--- http_req_res_simulation/src/main/java/com/Main.java
+++ http_req_res_simulation/src/main/java/com/Main.java
@@ -10,44 +10,49 @@ import java.net.Socket;
 public class Main {
     public static int PORT = 3000;
     public static void main(String[] args) throws IOException {
-        try (ServerSocket ss = new ServerSocket(PORT)) {
-            Socket s = ss.accept();
+        while(true){
+            try (ServerSocket ss = new ServerSocket(PORT)) {
+                Socket s = ss.accept();
 
-            PrintWriter out = new PrintWriter(s.getOutputStream(), true);
-            BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
-            String primaRiga  = in.readLine();
-            String request;
-            do {
-                request = in.readLine();
-                System.out.println(request);
-            } while (!request.isEmpty());
+                PrintWriter out = new PrintWriter(s.getOutputStream(), true);
+                BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+                String primaRiga  = in.readLine();
+                String request;
+                do {
+                    request = in.readLine();
+                    System.out.println(request);
+                } while (!request.isEmpty());
 
 
-            String [] parti = primaRiga.split(" ");
-            String path = parti[1];    
+                String [] parti = primaRiga.split(" ");
+                String path = parti[1];    
 
-            if("/ciao.html".equals(path)){
-                String response = "Buongiorno admin!";
-                out.println("HTTP/1.1 200 OK");
-                out.println("Content-Type: text/plain");
-                out.println("Content-Length: " + response.length());
-                out.println();
-                out.println(response);
-            } else if("/ciao".equals(path)){
-                out.println("HTTP/1.1 301 Moved Permanently");
-                out.println("Location: /ciao.html");
-                out.println("Content-Length: 0");
-                out.println();
-            } else{
-                String response = "404 Not Found";
-                out.println("HTTP/1.1 404 Not Found");
-                out.println("Content-Type: text/plain");
-                out.println("Content-Length: " + response.length());
-                out.println();
-                out.println(response);
+                if("/ciao.html".equals(path)){
+                    String response = "Buongiorno admin!";
+                    out.println("HTTP/1.1 200 OK");
+                    out.println("Content-Type: text/plain");
+                    out.println("Content-Length: " + response.length());
+                    out.println("Connection: keep-alive"); 
+                    out.println();
+                    out.println(response);
+                } else if("/ciao".equals(path)){
+                    out.println("HTTP/1.1 301 Moved Permanently");
+                    out.println("Location: /ciao.html");
+                    out.println("Content-Length: 0");
+                    out.println("Connection: keep-alive"); 
+                    out.println();
+                } else{
+                    String response = "404 Not Found";
+                    out.println("HTTP/1.1 404 Not Found");
+                    out.println("Content-Type: text/plain");
+                    out.println("Content-Length: " + response.length());
+                    out.println("Connection: keep-alive"); 
+                    out.println();
+                    out.println(response);
+                }
+
+                //s.close();
             }
-
-            s.close();
         }
     }
 }
